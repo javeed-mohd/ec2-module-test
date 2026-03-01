799ae6367bb7f830d75459c1e54e00f510fec4df
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-01 18:27:57 +0530

    actions

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,23 @@
+# module "ec2" {
+#     source          = "../terraform-aws-instance"
+#     project         = "roboshop"
+#     environment     = "dev"
+#     ami_id          = "ami-0220d79f3f480ecf5"
+#     instance_type   = "t3.large"
+#     sg_ids          = ["sg-0bdbfe5495a912910"]
+#     tags = {
+#         Name        = "roboshop-dev-catalogue"
+#     }
+# }
+
 module "ec2" {
     source          = "../terraform-aws-instance"
-    project         = "roboshop"
-    environment     = "dev"
+    project         = var.project_name # project/project_name can be used as we wish(Not necessarily it should be same but it gives the exact value required.)
+    environment     = var.env
     ami_id          = "ami-0220d79f3f480ecf5"
     instance_type   = "t3.large"
-    sg_ids          = ["sg-0bdbfe5495a912910"]
+    sg_ids          = var.sg_ids
     tags = {
-        Name        = "roboshop-dev-catalogue"
+        Name        = "${var.project_name}-${var.env}-${var.component}"
     }
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
-variable 
+variable "project_name" {
+    default  = "roboshop"
+}
+
+variable "env" {
+    default  = "dev"
+}
+
+variable "sg_ids" {
+    default  = ["sg-0bdbfe5495a912910"]
+}
+
+variable "component" {
+    default  = "catalogue"
+}
