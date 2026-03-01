0c3f27fb812e01ea0aa9cf9538b78b4855254595
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-01 18:19:13 +0530

    actions

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,11 @@
 module "ec2" {
     source          = "../terraform-aws-instance"
+    project         = "roboshop"
+    environment     = "dev"
     ami_id          = "ami-0220d79f3f480ecf5"
-    instance_type   = "t3.large" 
+    instance_type   = "t3.large"
+    sg_ids          = ["sg-0bdbfe5495a912910"]
+    tags = {
+        Name        = "roboshop-dev-catalogue"
+    }
 }
