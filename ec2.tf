1a0d6bebd1fb2fead1e3519703d97cbf88e319a5
Javeed Mohammed <javeedmohd214@gmail.com>
2026-03-02 17:51:28 +0530

    actions

--- ec2.tf
+++ ec2.tf
@@ -14,10 +14,11 @@ module "ec2" {
     source          = "../terraform-aws-instance"
     project         = var.project_name # project/project_name can be used as we wish(Not necessarily it should be same but it gives the exact default value provided.)
     environment     = var.env
-    ami_id          = "ami-0220d79f3f480ecf5"
+    ami_id          = data.aws_ami.joindevops.id
     instance_type   = "t3.large"
     sg_ids          = var.sg_ids
     tags = {
         Name        = "${var.project_name}-${var.env}-${var.component}"
+        Component   = var.component
     }
 }
